89085977069f034d7f65dc83b56b62f25713d2ec
AngelaFang <1823476439@qq.com>
2020-03-31 16:34:35 +0800

    将所有数据操作封装在store中 提交,测试有问题，在于window中的全局变量没有注释掉

--- src/main.ts
+++ src/main.ts
@@ -17,28 +17,28 @@ Vue.component('Layout',Layout);
 
 
 
-window.tagList = tagListModel.fetch();
-window.createTag = (name: string)=>{
-  const result = tagListModel.create(name);
-  if(result==="success"){
-    window.alert("创建成功")
-  }else if(result==="duplicated"){
-    window.alert("标签已存在")
-  }
-  console.log("2》window.createTag:",window.tagList);
-};
-window.deleteTag = (id)=>{
-  return tagListModel.delete(id);
-};
-window.updateTag = (id,name)=>{
-  return tagListModel.update(id,name);
-};
-window.findTag = (id)=>{
- return window.tagList.filter(item=>item.id===id)[0];
-};
-
-window.recordList = recordListModel.fetch();
-window.createRecord = (record: RecordItem)=>{return recordListModel.create(record)};
+// window.tagList = tagListModel.fetch();
+// window.createTag = (name: string)=>{
+//   const result = tagListModel.create(name);
+//   if(result==="success"){
+//     window.alert("创建成功")
+//   }else if(result==="duplicated"){
+//     window.alert("标签已存在")
+//   }
+//   console.log("2》window.createTag:",window.tagList);
+// };
+// window.deleteTag = (id)=>{
+//   return tagListModel.delete(id);
+// };
+// window.updateTag = (id,name)=>{
+//   return tagListModel.update(id,name);
+// };
+// window.findTag = (id)=>{
+//  return window.tagList.filter(item=>item.id===id)[0];
+// };
+//
+// window.recordList = recordListModel.fetch();
+// window.createRecord = (record: RecordItem)=>{return recordListModel.create(record)};
 
 new Vue({
   router,
